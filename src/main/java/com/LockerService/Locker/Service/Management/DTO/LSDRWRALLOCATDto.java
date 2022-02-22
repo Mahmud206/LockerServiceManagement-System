78f7ac701b768b986e149462cad3c50dda941337
Md. Sabbir Ahmed <41543394+SabbirAhmedSajib@users.noreply.github.com>
2022-02-22 11:57:55 +0600

    Update allocate Entity & Dto

--- src/main/java/com/LockerService/Locker/Service/Management/DTO/LSDRWRALLOCATDto.java
+++ src/main/java/com/LockerService/Locker/Service/Management/DTO/LSDRWRALLOCATDto.java
@@ -6,16 +6,34 @@ import java.sql.Date;
 
 @Data
 public class LSDRWRALLOCATDto {
+
     private long ALLOCATID;
+    private long LCKRID;
+    private long DRWRID;
+    private String OPRBRANCD;
+    private String BRANCD;
     private String ACTNUM;
     private String ACTTIT;
     private String ACTYPE;
-    private String  CUSCOD;
+    private String CUSCOD;
     private String PRDCOD;
+    private String OPRMOD;
+    private String SECREQ;
+    private String SECPAYTYP;
+    private String STATUS;
     private long SALEVAL;
     private long MARKETVAL;
-    private long  CAUTIONAMT;
+    private long CAUTIONAMT;
+    private long TENURE;
     private Date ALLOCATDATE;
     private Date EXPDATE;
     private String REMARKS;
+    private String OPRSTAMP;
+    private Date OPRTIMSTAMP;
+    private String UPDATE_BY;
+    private Date UPDATE_DATE;
+    private String APPFLG;
+    private String APPSTAMP;
+    private Date APPTIMSTAMP;
+    private String TRNFLG;
 }
--- src/main/java/com/LockerService/Locker/Service/Management/Entity/LSDRWRALLOCATEntity.java
+++ src/main/java/com/LockerService/Locker/Service/Management/Entity/LSDRWRALLOCATEntity.java
@@ -19,15 +19,32 @@ import java.sql.Date;
 public class LSDRWRALLOCATEntity {
     @Id
     private long ALLOCATID;
+    private long LCKRID;
+    private long DRWRID;
+    private String OPRBRANCD;
+    private String BRANCD;
     private String ACTNUM;
     private String ACTTIT;
     private String ACTYPE;
-    private String  CUSCOD;
+    private String CUSCOD;
     private String PRDCOD;
+    private String OPRMOD;
+    private String SECREQ;
+    private String SECPAYTYP;
+    private String STATUS;
     private long SALEVAL;
     private long MARKETVAL;
-    private long  CAUTIONAMT;
+    private long CAUTIONAMT;
+    private long TENURE;
     private Date ALLOCATDATE;
     private Date EXPDATE;
     private String REMARKS;
+    private String OPRSTAMP;
+    private Date OPRTIMSTAMP;
+    private String UPDATE_BY;
+    private Date UPDATE_DATE;
+    private String APPFLG;
+    private String APPSTAMP;
+    private Date APPTIMSTAMP;
+    private String TRNFLG;
 }
